72e91a1e3d9edfb3c0ab2a2aa7881378c6387d71
aldi-ovvl <aldi1529y@gmail.com>
2026-04-22 09:54:55 +0800

    Update materi2.php

--- materi2.php
+++ materi2.php
@@ -14,3 +14,27 @@ if (isset($_post["angka"])) {
 }
 
 ?>
+
+// Contoh perulangan for nested (for i c)
+echo "<h3>Contoh Perulangan For Nested (i dan c):</h3>";
+for ($i = 1; $i <= 3; $i++) {
+    for ($c = 1; $c <= 3; $c++) {
+        echo "i=$i, c=$c <br>";
+    }
+}
+
+// Contoh perulangan while
+echo "<h3>Contoh Perulangan While:</h3>";
+$i = 1;
+while ($i <= 5) {
+    echo "Nilai while: $i <br>";
+    $i++;
+}
+
+// Contoh perulangan do-while
+echo "<h3>Contoh Perulangan Do-While:</h3>";
+$i = 1;
+do {
+    echo "Nilai do-while: $i <br>";
+    $i++;
+} while ($i <= 5);
